87f81a1e5c8bb2ca776e4cce20e25c5dd34e3242
Sunil <sunil10790@gmail.com>
2020-04-06 22:36:41 +0530

    Serve static files from node modules

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var app = express();
 
 app.use(morgan('tiny'));
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css' , express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/@popperjs/core/dist/umd')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname , '/views/index.html'));
 });
