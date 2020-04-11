2bd3ff8acfd0c6467db19d58ba91a81c1d1b1fec
Sunil <sunil10790@gmail.com>
2020-04-11 09:31:34 +0530

    Install and Configure nodemon

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const morgan = require('morgan');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -20,6 +22,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`Listening to Port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`Listening at Port ${chalk.green(port)}`);
 });
