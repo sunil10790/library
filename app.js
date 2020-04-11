a7314242af0e5541e8bc856947549fed66fd8030
Sunil <sunil10790@gmail.com>
2020-04-11 09:05:31 +0530

    Fix ESLint issues

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/css' , express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/@popperjs/core/dist/umd')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname , '/views/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.listen(3000, function() {
-    debug(`Listening to Port ${chalk.green('3000')}`);
+app.listen(3000, function () {
+  debug(`Listening to Port ${chalk.green('3000')}`);
 });
