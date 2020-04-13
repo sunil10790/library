dbc8e6d5114cebd1b08db1cf70aa9084d34de274
Sunil <sunil10790@gmail.com>
2020-04-13 08:30:20 +0530

    Install and Setup Pug

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const morgan = require('morgan');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,12 +14,14 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/@popperjs/core/dist/umd')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index');
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   debug(`Listening at Port ${chalk.green(port)}`);
 });
